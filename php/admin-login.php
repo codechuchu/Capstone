5888de26e3c51a6670858561ce696af5b48f64da
codechuchu <onionknight418@gmail.com>
2025-10-05 13:41:21 +0800

    Update admin-login.php
    editing database credentials

--- php/admin-login.php
+++ php/admin-login.php
@@ -9,7 +9,8 @@ session_set_cookie_params([
 session_start();
 header('Content-Type: application/json');
 
-$conn = new mysqli("localhost", "root", "Sulivannhs2025", "sulivannhs");
+$conn = new mysqli("localhost", "u705788258_sulivan", "Sulivannhs2025", "u705788258_sulivannhs");
+
 if ($conn->connect_error) {
     http_response_code(500);
     echo json_encode(["success" => false, "message" => "Database connection failed"]);
